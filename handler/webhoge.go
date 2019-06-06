61510c52c0e2f239034effc43f807129186be5fb
task-mizushima <t.mizushima@april-knights.com>
2019-06-06 21:52:37 +0900

    add directory handler,usecase,domain

---
+++ handler/webhoge.go
@@ -0,0 +1,18 @@
+package handler
+package templates
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func http() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+	router.Run(":8080")
+}
